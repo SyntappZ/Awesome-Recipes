e321cc59c5d7764ef174b4bbd8fe0830770f4cbb
syntappz <doddsanater87@gmail.com>
2019-11-04 16:09:09 +0000

    new drop menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 // import { getData } from "./recipeData.js";
 force.bindHashes();
 
- 
-
-const cuisineType = [
+let cuisineTypeElement = document.querySelector(".cuisine-list");
+let cuisineList = [
   "American",
   "Asian",
   "British",
@@ -23,6 +22,67 @@ const cuisineType = [
   "South American",
   "South East Asian"
 ];
+cuisineList.forEach(x => {
+  cuisineTypeElement.innerHTML += `<li>${x}</li>`;
+});
+
+//meal type
+let mealType = document.querySelector(".meal-list");
+["Breakfast", "Lunch", "Dinner", "Snack"].forEach(x => {
+  mealType.innerHTML += `<li onclick="getMeal(${x})">${x}</li>`;
+});
+
+function getMeal(meal) {
+  console.log(meal)
+}
+
+//dish type
+
+let dishTypeElement = document.querySelector(".dish-list");
+
+let dishList = [
+  'Bread',
+	'Cereals',
+	'sauces',
+	'Drinks',
+	'Desserts',
+	'Main course',
+	'Pancake',
+	'Preps',
+	'Preserve',
+	'Salad',
+	'Sandwiches',
+	'Side dish',
+	'Soup',
+	'Starter',
+	'Sweets',
+];
+
+dishList.forEach(x => {
+  dishTypeElement.innerHTML += `<li>${x}</li>`;
+});
+
+//diet
+
+let dietTypeElement = document.querySelector(".diet-list");
+
+
+let dietList = [
+ 	'balanced',	
+	'high-fiber',
+	'High-Protein',
+	'Low-Carb',
+	'Low-Fat',
+	'Low-Sodium',
+];
+
+dietList.forEach(x => {
+  dietTypeElement.innerHTML += `<li>${x}</li>`;
+});
+
+
+
+
 
 // btn.addEventListener("click", () => {
 //   const recipes = getData("fish", cuisineType[2]);
